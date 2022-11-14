c2b0cd453d00d6da0a380816144349220c29bbc2
Moahmedahmadian <42762811+Moahmedahmadian@users.noreply.github.com>
2022-11-14 12:16:43 +0330

    change userimage path

--- src/component/users/userSlice.js
+++ src/component/users/userSlice.js
@@ -1,23 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
+const userImage = "https://lh3.googleusercontent.com/ogw/AOh-ky1UFfrEQvUD8SpDkU62gBVyDSVoAaDEwQH2i2KbGA=s32-c-mo"
 const UserSlice = createSlice({
     name: "user",
     initialState: {
         isAuth: false,
-        accessToken: "123",
+        accessToken: "",
         userInfo: {
             name: "",
-            image: "https://lh3.googleusercontent.com/ogw/AOh-ky1UFfrEQvUD8SpDkU62gBVyDSVoAaDEwQH2i2KbGA=s32-c-mo"
+            image: ""
         }
     },
     reducers: {
         login: (state, action) => {
             state.isAuth = true;
             state.userInfo.name = action.payload.name
+            state.userInfo.image = userImage
         },
         logout: (state) => {
             state.isAuth = false;
             state.userInfo = {
-                image: "https://lh3.googleusercontent.com/ogw/AOh-ky1UFfrEQvUD8SpDkU62gBVyDSVoAaDEwQH2i2KbGA=s32-c-mo"
             }
         }
     }
